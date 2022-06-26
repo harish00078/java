da2c66a16293c40a99f78ccca35993c4dd5e0f94
harish kumar <harishkunar987@gmail.com>
2022-06-26 21:54:24 +0530

    upgrade

--- Main.java
+++ Main.java
@@ -1,8 +1,43 @@
 public class Main{
     public static void main(String[] args) {
         //write code here
-        System.out.println("hello world");
-        System.out.print("my name is harish");
+        System.out.println("hello world"); // println = ln represent here to jump on next line 
+        System.out.println("my name is harish");
+        /*
+         * variables
+         * just like:
+         * for water - webucket
+         * for store masala - we use boxes
+         * for storing lunch - we use lunchbox
+         * In java:
+         * variables are contains which store data values:
+         * string ex("string"),int ex(1,2,3),float ex(0.1,0.2),char ex('harish'),boolean ex(true,false)
+         * 
+         * # how to declare variables:
+         * syntax - <datatype> <variableName> = value;
+         */
+        String name = "Harry";
+        System.out.println(name);
         
+        int a = 45, x=56, y=67;
+        float b = 45.22f; // In java we have define the float value by writing a (f) with the value.
+        
+        boolean isadult = false;
+        System.out.println(y);
+        System.out.println(b);
+        
+        System.out.println(isadult);
+
+        /*
+         * rules for constructing name of variables in java
+         * 1. can contain digits, underscore, dollar signs, letters
+         * 2. should begin with a letter, $ or _
+         * 3. java is case sensitive language which means that harry and Harry are two different variables altogether        
+         * 4. should not contain whitespaces
+         * 5. you cannot use reserved keywords from java
+         * 
+         * */
+
+
     }
 }
